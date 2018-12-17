08467c2d984df75f99aad3c03a6565290058fbd1
Matt Agostinelli <matthew.agostinelli@gmail.com>
2018-12-17 02:41:37 -0500

    almost there on fitness

--- Utils.py
+++ Utils.py
@@ -2,53 +2,48 @@ import numpy as np
 import pandas as pd
 import os
 import time
-
+import kaggle
 
 class Graph:
     def __init__(self, vx, vy, seed_path=None):
         self.n = len(vx)
+        self.bitpen = (np.mod(np.arange(1, self.n + 1), 10) == 0) * 1
 
-        #Set permutation to take place on cities 2-n
+        # Set permutation to include cities 1:n, exclusive of Zero
         if seed_path is None:
-            self.path = np.concatenate([[0],np.random.permutation(np.arange(1,self.n))])
+            self.path = np.random.permutation(self.n - 1) + 1
         else:
             self.path = seed_path
-        print(self.path)
+
         self.x = vx
         self.y = vy
 
         self.prime = sieve(self.n)
-        self._fitness = self.calc_fitness()
-
-    def get_fitness(self):
-        print("Getting Fitness Value")
-        return self._fitness
+        self.fitness = self.calc_fitness()
 
     def calc_fitness(self):
-        _z = -np.argmin(self.path)
-        print(len(self.prime),len(self.path))
-        _penalty = (np.clip(np.mod(np.arange(self.n), 9), 0, 1) ^ 1) * (self.prime[self.path])
-        _penalty[0] = _penalty[1] = 0
-        _penalty = np.clip(_penalty + 1.0, 1.0, 1.1)
-        self._fitness = np.sum(
+        _penalty = np.clip((self.bitpen * self.prime[np.concatenate(([0], self.path))]) + 1, 1.0, 1.1)
+        _fitness = np.sum(
             np.sqrt(
                 _penalty
                 * (
-                        np.power(self.x[np.roll(self.path, _z)] -
-                                 self.x[np.roll(self.path, _z - 1)], 2)
+                        np.power(self.x[np.concatenate(([0], self.path))] -
+                                 self.x[np.concatenate((self.path, [0]))], 2)
 
-                        + np.power(self.y[np.roll(self.path, _z)] -
-                                    self.y[np.roll(self.path, _z - 1)], 2)
+                        + np.power(self.y[np.concatenate(([0], self.path))] -
+                                   self.y[np.concatenate((self.path, [0]))], 2)
                 )
             )
         )
-        return self._fitness
-    def Submit_File(self,file,msg='',upload=False):
-        #Prepended 0 to the end on the path
-        pd.DataFrame({'Path':np.concatenate([self.path,[0]])}).to_csv(file,index=False)
+        return _fitness
+
+    def Submit_File(self, file, msg='', upload=False):
+        # Prepended 0 to the end on the path
+        pd.DataFrame({'Path': np.concatenate(([0], self.path, [0]))}).to_csv(file, index=False)
         if upload:
-            command = f'kaggle competitions submit -c traveling-santa-2018-prime-paths -f {file} -m "{msg}"'
+            command = "kaggle competitions submit -c traveling-santa-2018-prime-paths -f {file} -m \"{msg}\""
             os.system(command)
+
 
 def sieve(n):
     flags = np.ones(n, dtype=int)
@@ -63,9 +58,11 @@ def sieve(n):
 
 if __name__ == '__main__':
     raw = pd.read_csv(os.path.join(os.getcwd(), 'Data', 'cities.csv'))
+    pth = pd.read_csv(os.path.join(os.getcwd(), 'Sub.csv'))
+    pth = pth['Path'][1:-1].values
     _t0 = time.time()
-    tsp = Graph(raw['X'].values, raw['Y'].values)
+    tsp = Graph(raw['X'].values, raw['Y'].values, pth)
     _t1 = time.time()
     print(_t1 - _t0)
-    print(tsp._fitness)
-    tsp.Submit_File(os.path.join(os.getcwd(),'Sub.csv'),msg='Test Matts Fitness',upload=False)
+    print(tsp.fitness)
+    tsp.Submit_File(os.path.join(os.getcwd(), 'Sub.csv'), msg='Test Matts Fitness', upload=False)
