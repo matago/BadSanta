7b2f1063681301bc7b61d7678b5270856c6324e5
Matt Agostinelli <matthew.agostinelli@gmail.com>
2018-12-18 02:40:22 -0500

    fitness with zeros excluded from path --and fast

--- Utils.py
+++ Utils.py
@@ -2,68 +2,79 @@ import numpy as np
 import pandas as pd
 import os
 import time
+import kaggle
+
 
 class Graph:
     def __init__(self, vx, vy, seed_path=None):
         self.n = len(vx)
-        self.bitpen = (np.mod(np.arange(1, self.n+1), 10) == 0) * 1
-        print(self.bitpen[0:21])
+
+        self.bitpen = (np.mod(np.arange(1, self.n + 1), 10) == 0) * 1
+
         # Set permutation to include cities 1:n, exclusive of Zero
         if seed_path is None:
-            self.path = np.random.permutation(self.n-1) + 1
+            self.path = np.random.permutation(self.n - 1) + 1
         else:
             self.path = seed_path
 
         self.x = vx
         self.y = vy
 
-        self.prime = sieve(self.n)
-        print(self.prime[0:21])
+        self.prime = primesfrom2to(self.n + 2)
+
         self.fitness = self.calc_fitness()
 
     def calc_fitness(self):
-        _penalty = np.clip((self.bitpen * self.prime[np.concatenate(([0], self.path))] + 1), 1.0, 1.1)
-        print(_penalty[0:21])
-        _fitness = np.sum(
-            np.sqrt(
-                _penalty
-                * (
-                        np.power(self.x[np.concatenate(([0], self.path))] -
-                                 self.x[np.concatenate((self.path, [0]))], 2)
+        _penalty = np.clip(self.bitpen * np.isin(np.concatenate(([0], self.path)), self.prime, invert=True) * 2, 1, 1.1)
 
-                        + np.power(self.y[np.concatenate(([0], self.path))] -
-                                   self.y[np.concatenate((self.path, [0]))], 2)
+        _fitness = np.sum( _penalty * np.sqrt(
+                        np.power(
+                            self.x[np.concatenate(([0], self.path))] -
+                            self.x[np.concatenate((self.path, [0]))]
+                            , 2)
+
+                        + np.power(
+                            self.y[np.concatenate(([0], self.path))] -
+                            self.y[np.concatenate((self.path, [0]))], 2)
                 )
             )
-        )
         return _fitness
 
     def Submit_File(self, file, msg='', upload=False):
         # Prepended 0 to the end on the path
         pd.DataFrame({'Path': np.concatenate(([0], self.path, [0]))}).to_csv(file, index=False)
         if upload:
-            command = "kaggle competitions submit -c traveling-santa-2018-prime-paths -f {file} -m \"{msg}\""
+            command = f"kaggle competitions submit -c traveling-santa-2018-prime-paths -f {file} -m \"{msg}\""
             os.system(command)
 
+    def tour_plot(self):
+        output_file("line.html")
+        p = figure(plot_width=1000, plot_height=1000)
+        p.line(self.x, self.y, line_width=.05)
+        p.circle(self.x[self.prime == 0], self.y[self.prime == 0], size=3, color='black')
+        show(p)
 
-def sieve(n):
-    flags = np.ones(n, dtype=int)
-    flags[0] = flags[1] = 0
-    for i in range(2, n):
-        # We could use a lower upper bound for this loop, but I don't want to bother with
-        # getting the rounding right on the sqrt handling.
-        if flags[i] == 1:
-            flags[i * i::i] = 0
-    return flags ^ 1
+
+def primesfrom2to(n):
+    """ Input n>=6, Returns a array of primes, 2 <= p < n """
+    sieve = np.ones(n // 3 + (n % 6 == 2), dtype=np.bool)
+    sieve[0] = False
+    for i in range(int(n ** 0.5) // 3 + 1):
+        if sieve[i]:
+            k = 3 * i + 1 | 1
+            sieve[((k * k) // 3)::2 * k] = False
+            sieve[(k * k + 4 * k - 2 * k * (i & 1)) // 3::2 * k] = False
+    return np.r_[2, 3, ((3 * np.nonzero(sieve)[0] + 1) | 1)]
 
 
 if __name__ == '__main__':
     raw = pd.read_csv(os.path.join(os.getcwd(), 'Data', 'cities.csv'))
-    pth = pd.read_csv(os.path.join(os.getcwd(), 'Sub.csv'))
+    pth = pd.read_csv(os.path.join(os.getcwd(), 'Data', 'sample_submission.csv'))
     pth = pth['Path'][1:-1].values
+    print(pth)
     _t0 = time.time()
     tsp = Graph(raw['X'].values, raw['Y'].values, pth)
     _t1 = time.time()
     print(_t1 - _t0)
     print(tsp.fitness)
-    tsp.Submit_File(os.path.join(os.getcwd(), 'Sub.csv'), msg='Test Matts Fitness', upload=False)
+    tsp.Submit_File(os.path.join(os.getcwd(), 'Data','sample_submission.csv'), msg='Test Matts Fitness', upload=False)
