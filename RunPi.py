39dbbda26f271de950f71fdd0627f22bef6852f0
dkowalsky7 <dkowalsky7@gmail.com>
2018-12-20 17:03:24 -0500

    Simple Setup to Run on the Pi, forgot entry info

--- RunPi.py
+++ RunPi.py
@@ -5,13 +5,15 @@ from Anneal import anneal
 from time import time
 
 print(os.getcwd())
+
+it = input('How Many Iterations should i run? (must be an int!)')
 raw = pd.read_csv(os.path.join(os.getcwd(), 'Data', 'cities.csv'))
 
 
 a = anneal(raw['X'].values, raw['Y'].values)
 o = float(a.fitness)
 st = time()
-r = a.cool(alpha=0.95,T=1,Tmin=0.0001,iters=2)
+r = a.cool(alpha=0.95,T=1,Tmin=0.0001,iters=int(it))
 print(time()-st)
 print(f'Original Fitness = {o}')
 print(f'New Fitness = {a.calc_fitness()}')
